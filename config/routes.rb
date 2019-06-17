97d4618dfee2f4a6e1b4fd47ffa0358b0ed1a618
Matt Williams <matt.seongjun@gmail.com>
2019-06-17 14:03:05 -0400

    Bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
     resources :users
-    resources :wallets, :currency, :trades only: [:index, :show, :new, :create]
+    resources :wallets, :currency, :trades, only: [:index, :show, :new, :create]
 end
---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_06_17_162652) do
+
+  create_table "currencies", force: :cascade do |t|
+    t.integer "name"
+    t.string "symbol"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.float "offered_amount"
+    t.integer "offered_wallet_id"
+    t.float "requested_amount"
+    t.integer "requested_wallet_id"
+    t.boolean "complete?"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "currency_id"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
