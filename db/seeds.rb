e7f9f72cc98e8aac609c1bf5b696fec0af767760
Matt Williams <matt.seongjun@gmail.com>
2019-06-17 14:26:15 -0400

    Make seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Currency.destroy_all
+Wallet.destroy_all
+Trade.destroy_all
+
+matt = User.create(name: "Matt")
+errol = User.create(name: "Errol")
+
+bitcoin = Currency.create(name: "Bitcoin", symbol: "BTC")
+dollar = Currency.create(name: "United States Dollar", symbol: "USD")
+
+errol_wallet = Wallet.create(user_id: errol.id, currency_id: bitcoin.id, amount: 5)
+matt_wallet = Wallet.create(user_id: matt.id, currency_id: dollar.id, amount: 10000)
