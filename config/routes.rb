f37b9b69f92b45abbf7662cf5fb9c27aadc37d40
Matt Williams <matt.seongjun@gmail.com>
2019-06-21 10:34:59 -0400

    Remove trade show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
     resources :currencies
     resources :users, except: :index
-    resources :wallets, :trades, except: [:edit, :update]
+    resources :wallets, except: [:edit, :update]
+    resources :trades, except: [:show, :edit, :update] 
     get "/login", to: "sessions#new", as: "login"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
